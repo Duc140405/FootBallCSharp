aaf6f7467bba59bc1e0458ca113c491693bf9ec3
Nguyen Quoc Do <nguyenquocdodary@gmail.com>
2026-01-29 14:51:36 +0700

    Tao form thanh cong

---
+++ Football_Management_System/Football_Management_System/StandingsWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace Football_Management_System
+{
+    public partial class StandingsWindow : Window
+    {
+        public StandingsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
